b2b91d635c1fda0b52d34adfac42bb581c12b155
kanoi-y <furafuratonbo@gmail.com>
2021-12-21 00:13:44 +0900

    ✨ mockを作成

---
+++ __tests__/NavBar.test.tsx
@@ -0,0 +1,47 @@
+import '@testing-library/jest-dom/extend-expect'
+import { cleanup, render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { setupServer } from 'msw/node'
+import { getPage, initTestHelpers } from 'next-page-tester'
+import 'setimmediate'
+import { handlers } from '../mock/handlers'
+
+
+initTestHelpers()
+
+const server = setupServer(...handlers)
+
+beforeAll(() => {
+  server.listen()
+})
+afterEach(() => {
+  server.resetHandlers()
+  cleanup()
+})
+afterAll(() => {
+  server.close()
+})
+
+describe('Navigation Test Cases', () => {
+  it('Should route to selected page in navbar', async () => {
+    const { page } = await getPage({
+      route: '/',
+    })
+    render(page)
+    expect(await screen.findByText('Next.js + GraphQL')).toBeInTheDocument()
+    userEvent.click(screen.getByTestId('makevar-nav'))
+    expect(await screen.findByText('makeVar')).toBeInTheDocument()
+    userEvent.click(screen.getByTestId('fetchpolicy-nav'))
+    expect(await screen.findByText('Hasura main page')).toBeInTheDocument()
+    userEvent.click(screen.getByTestId('crud-nav'))
+    expect(await screen.findByText('Hasura CRUD')).toBeInTheDocument()
+    userEvent.click(screen.getByTestId('ssg-nav'))
+    expect(await screen.findByText('SSG+ISR')).toBeInTheDocument()
+    userEvent.click(screen.getByTestId('memo-nav'))
+    expect(
+      await screen.findByText('Custom Hook + useCallback + memo')
+    ).toBeInTheDocument()
+    userEvent.click(screen.getByTestId('home-nav'))
+    expect(await screen.findByText('Next.js + GraphQL')).toBeInTheDocument()
+  })
+})
